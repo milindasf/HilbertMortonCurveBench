51bc2a0a4b74b061f3bf938f52e8f8f3d11c07ce
milindasf@gmail.com <milindasf@gmail.com>
2015-10-11 11:52:22 -0600

    main.cpp intilize function changed

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include "include/benchmark.h"
-#include "include/rotation.h"
+
 
 
 int main(int argc, char **argv) {
     std::cout<<"Hilbert Morton Bench Version 2.0"<<std::endl;
     Benchmark* bench;
-       
+
     int args=argc;
     int arg_dim=atoi(argv[1]);
     int arg_num_pts=atoi(argv[2]);
     int arg_max_depth=atoi(argv[3]);
-    int arg_iter=atoi(argv[4]);   
-    initializeHilbetTable(2);
-    initializeHilbetTable(3);
-    
-    
+    int arg_iter=atoi(argv[4]);
+
+    //initializeHilbetTable(arg_dim);
+
+    _InitializeHcurve();
+
     bench=new Benchmark(arg_max_depth,arg_dim,arg_num_pts,arg_iter);
     bench->run_bench();
     free(bench);
